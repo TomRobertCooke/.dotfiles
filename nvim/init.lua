29f3611b58984cf3807504e8d5a73238a575571d
Thomas Cooke <tom.robert.cooke@gmail.com>
2026-05-16 21:18:16 -0400

    changed alpha welcome page for nvim, tmux theme tweaks

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,10 +3,13 @@ vim.g.maplocalleader = "\\"
 
 require("config.lazy")
 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = false
+vim.opt.autoindent = false
+vim.opt.cindent = false
 
 require("config.lsp-configs")
 require("config.rebinds")
